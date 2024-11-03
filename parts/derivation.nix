c3b853519d5f02a1e63b68244b0209cfb94398e4
uku <hi@uku.moe>
2024-11-03 14:32:43 +0100

    feat: use mold as linker

--- parts/derivation.nix
+++ parts/derivation.nix
@@ -1,15 +1,20 @@
 {
   lib,
   rustPlatform,
+  mold-wrapped,
   self,
 }:
 rustPlatform.buildRustPackage {
   pname = "ukubot-rs";
   version = self.shortRev or self.dirtyShortRev or "dirty";
 
+  src = self;
+
+  nativeBuildInputs = [mold-wrapped];
+
   cargoLock.lockFile = ../Cargo.lock;
 
-  src = self;
+  RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
 
   meta = with lib; {
     mainProgram = "ukubot-rs";
